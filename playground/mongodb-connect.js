9f4d075ca4c06fd631669f912eb48abe62a44075
Adrian Gonzalez <adrianandgonzalez@gmail.com>
2018-06-04 14:56:34 -0700

    initial commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,43 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+// var obj = new ObjectID();
+// console.log(obj);
+
+
+// var user = {name: 'Adrian', age: 25};
+// var {name} = user;
+// console.log(name);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if(err){
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+  const db = client.db('TodoApp');
+
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed: false
+  // }, (err, result) => {
+  //   if(err){
+  //     return console.log('Unable able to insert in todo', err);
+  //   }
+  //
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
+
+  // db.collection('Users').insertOne({
+  //   name: 'Adrian',
+  //   age: 25,
+  //   location: 'Los Angeles'
+  // }, (err, result) => {
+  //   if(err){
+  //     return console.log('Unable able to insert in todo', err);
+  //   }
+  //
+  //   console.log(result.ops[0]._id.getTimestamp());
+  // });
+
+  client.close();
+});
