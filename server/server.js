b2e2e7bb79a6eb4a4739b328ec6341fb91c587ad
Adrian Gonzalez <adrianandgonzalez@gmail.com>
2018-06-12 20:22:44 -0700

    added delete route

--- server/server.js
+++ server/server.js
@@ -48,6 +48,23 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if(!todo){
+      return res.status(404).send();
+    }
+      res.send(`Deleted ${todo.text}`);
+  }).catch((error) => {
+    res.status(400).send();
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server starting on Port: ${port}`);
 });
